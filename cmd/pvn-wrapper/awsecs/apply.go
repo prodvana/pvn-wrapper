137df4501f871b284db8cb96ce5c1004961ca396
Naphat Sanguansin <naphat@prodvana.io>
2023-11-29 18:27:32 +0000

    refactor out common flags

--- cmd/pvn-wrapper/awsecs/apply.go
+++ cmd/pvn-wrapper/awsecs/apply.go
@@ -19,18 +19,6 @@ const (
 	serviceVersionTagKey = "pvn:version"
 )
 
-var applyFlags = struct {
-	taskDefinitionFile string
-	ecsClusterName     string
-	ecsServiceName     string
-	pvnServiceId       string
-	pvnServiceVersion  string
-	desiredCount       int
-	subnets            []string
-	securityGroups     []string
-	assignPublicIp     bool
-}{}
-
 type getResourcesOutput struct {
 	ResourceTagMappingList []struct {
 		ResourceARN string `json:"ResourceARN"`
@@ -200,16 +188,16 @@ var applyCmd = &cobra.Command{
 	Short: "Create or update an ECS service",
 	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		newTaskDefPath, err := patchTaskDefinition(applyFlags.taskDefinitionFile, applyFlags.pvnServiceId, applyFlags.pvnServiceVersion)
+		newTaskDefPath, err := patchTaskDefinition(commonFlags.taskDefinitionFile, commonFlags.pvnServiceId, commonFlags.pvnServiceVersion)
 		if err != nil {
 			return err
 		}
 		defer func() { _ = os.Remove(newTaskDefPath) }()
-		serviceOutput, err := describeService(applyFlags.ecsClusterName, applyFlags.ecsServiceName)
+		serviceOutput, err := describeService(commonFlags.ecsClusterName, commonFlags.ecsServiceName)
 		if err != nil {
 			return err
 		}
-		taskArn, err := registerTaskDefinitionIfNeeded(newTaskDefPath, applyFlags.pvnServiceId, applyFlags.pvnServiceVersion, serviceOutput)
+		taskArn, err := registerTaskDefinitionIfNeeded(newTaskDefPath, commonFlags.pvnServiceId, commonFlags.pvnServiceVersion, serviceOutput)
 		if err != nil {
 			return err
 		}
@@ -223,31 +211,31 @@ var applyCmd = &cobra.Command{
 			}
 		}
 		networkConfigurations := []string{
-			fmt.Sprintf("subnets=[%s]", strings.Join(applyFlags.subnets, ",")),
-			fmt.Sprintf("securityGroups=[%s]", strings.Join(applyFlags.securityGroups, ",")),
+			fmt.Sprintf("subnets=[%s]", strings.Join(commonFlags.subnets, ",")),
+			fmt.Sprintf("securityGroups=[%s]", strings.Join(commonFlags.securityGroups, ",")),
 		}
-		if applyFlags.assignPublicIp {
+		if commonFlags.assignPublicIp {
 			networkConfigurations = append(networkConfigurations, "assignPublicIp=ENABLED")
 		}
 		commonArgs := []string{
 			"--task-definition",
 			taskArn,
 			"--desired-count",
-			fmt.Sprintf("%d", applyFlags.desiredCount),
+			fmt.Sprintf("%d", commonFlags.desiredCount),
 			"--network-configuration",
 			fmt.Sprintf("awsvpcConfiguration={%s}", strings.Join(networkConfigurations, ",")),
 			"--propagate-tags=TASK_DEFINITION",
 			"--cluster",
-			applyFlags.ecsClusterName,
+			commonFlags.ecsClusterName,
 		}
 		if serviceOutput.Services[0].Status == "INACTIVE" || serviceOutput.Services[0].Status == "MISSING" {
-			log.Printf("Creating service %s on cluster %s with task ARN %s\n", applyFlags.ecsServiceName, applyFlags.ecsClusterName, taskArn)
+			log.Printf("Creating service %s on cluster %s with task ARN %s\n", commonFlags.ecsServiceName, commonFlags.ecsClusterName, taskArn)
 			// create service
 			createCmd := exec.Command(awsPath, append([]string{
 				"ecs",
 				"create-service",
 				"--service-name",
-				applyFlags.ecsServiceName,
+				commonFlags.ecsServiceName,
 				"--launch-type=FARGATE",
 			}, commonArgs...)...)
 			err := cmdutil.RunCmd(createCmd)
@@ -255,20 +243,20 @@ var applyCmd = &cobra.Command{
 				return err
 			}
 		} else {
-			log.Printf("Updating service %s on cluster %s with task ARN %s\n", applyFlags.ecsServiceName, applyFlags.ecsClusterName, taskArn)
+			log.Printf("Updating service %s on cluster %s with task ARN %s\n", commonFlags.ecsServiceName, commonFlags.ecsClusterName, taskArn)
 			// update service
 			updateCmd := exec.Command(awsPath, append([]string{
 				"ecs",
 				"update-service",
 				"--service",
-				applyFlags.ecsServiceName,
+				commonFlags.ecsServiceName,
 			}, commonArgs...)...)
 			err := cmdutil.RunCmd(updateCmd)
 			if err != nil {
 				return err
 			}
 		}
-		waitCmd := exec.Command(awsPath, "ecs", "wait", "services-stable", "--services", applyFlags.ecsServiceName, "--cluster", applyFlags.ecsClusterName)
+		waitCmd := exec.Command(awsPath, "ecs", "wait", "services-stable", "--services", commonFlags.ecsServiceName, "--cluster", commonFlags.ecsClusterName)
 		return cmdutil.RunCmd(waitCmd)
 	},
 }
@@ -276,22 +264,5 @@ var applyCmd = &cobra.Command{
 func init() {
 	RootCmd.AddCommand(applyCmd)
 
-	applyCmd.Flags().StringVar(&applyFlags.taskDefinitionFile, "task-definition-file", "", "Path to task definition file")
-	cmdutil.Must(applyCmd.MarkFlagRequired("task-definition-file"))
-	applyCmd.Flags().StringVar(&applyFlags.ecsServiceName, "ecs-service-name", "", "Name of ECS service")
-	cmdutil.Must(applyCmd.MarkFlagRequired("ecs-service-name"))
-	applyCmd.Flags().StringVar(&applyFlags.ecsClusterName, "ecs-cluster-name", "", "Name of ECS cluster")
-	cmdutil.Must(applyCmd.MarkFlagRequired("ecs-cluster-name"))
-	applyCmd.Flags().StringVar(&applyFlags.pvnServiceId, "pvn-service-id", "", "Prodvana Service ID")
-	cmdutil.Must(applyCmd.MarkFlagRequired("pvn-service-id"))
-	applyCmd.Flags().StringVar(&applyFlags.pvnServiceVersion, "pvn-service-version", "", "Prodvana Service Version")
-	cmdutil.Must(applyCmd.MarkFlagRequired("pvn-service-version"))
-	applyCmd.Flags().IntVar(&applyFlags.desiredCount, "desired-count", 0, "Number of instances desired")
-	cmdutil.Must(applyCmd.MarkFlagRequired("desired-count"))
-	applyCmd.Flags().StringSliceVar(&applyFlags.subnets, "subnets", nil, "Subnets to use")
-	cmdutil.Must(applyCmd.MarkFlagRequired("subnets"))
-	applyCmd.Flags().StringSliceVar(&applyFlags.securityGroups, "security-groups", nil, "Security groups to use")
-	cmdutil.Must(applyCmd.MarkFlagRequired("security-groups"))
-	applyCmd.Flags().BoolVar(&applyFlags.assignPublicIp, "assign-public-ip", false, "Assign public IP")
-	cmdutil.Must(applyCmd.MarkFlagRequired("assign-public-ip"))
+	registerCommonFlags(applyCmd)
 }
